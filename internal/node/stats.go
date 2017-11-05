1b1dd557ccb91b77ac897547e52f841e099c96d8
callpraths <callpraths@gmail.com>
2017-11-05 11:26:47 -0800

    Implement CoundNodes for ROBDDs with shared nodes.

--- internal/node/stats.go
+++ internal/node/stats.go
@@ -5,15 +5,25 @@ import (
 )
 
 func CountNodes(n *Node) (int, error) {
+	m := make(map[*Node]bool)
+	return countNodesHelper(n, m)
+}
+
+func countNodesHelper(n *Node, m map[*Node]bool) (int, error) {
+	_, seen := m[n]
+	if seen {
+		return 0, nil
+	}
+	m[n] = true
 	switch n.Type {
 	case LeafType:
 		return 1, nil
 	case InternalType:
-		t, et := CountNodes(n.True)
+		t, et := countNodesHelper(n.True, m)
 		if et != nil {
 			return t, et
 		}
-		f, ef := CountNodes(n.False)
+		f, ef := countNodesHelper(n.False, m)
 		if ef != nil {
 			return f, ef
 		}
@@ -21,4 +31,5 @@ func CountNodes(n *Node) (int, error) {
 	default:
 		return -1, fmt.Errorf("Malformed node: %v", n)
 	}
+
 }
--- stats_test.go
+++ stats_test.go
@@ -18,7 +18,7 @@ func TestCountTreeStructure(t *testing.T) {
 				&node.Node{
 					Type: node.InternalType,
 					Internal: node.Internal{
-						Ply:   1,
+						Ply:   0,
 						True:  &node.Node{Type: node.LeafType, Leaf: node.Leaf{true}},
 						False: &node.Node{Type: node.LeafType, Leaf: node.Leaf{false}},
 					},
@@ -32,11 +32,11 @@ func TestCountTreeStructure(t *testing.T) {
 				&node.Node{
 					Type: node.InternalType,
 					Internal: node.Internal{
-						Ply: 1,
+						Ply: 0,
 						True: &node.Node{
 							Type: node.InternalType,
 							Internal: node.Internal{
-								Ply:   2,
+								Ply:   1,
 								True:  &node.Node{Type: node.LeafType, Leaf: node.Leaf{false}},
 								False: &node.Node{Type: node.LeafType, Leaf: node.Leaf{false}},
 							},
@@ -56,5 +56,83 @@ func TestCountTreeStructure(t *testing.T) {
 		if c != tt.c {
 			t.Errorf("CountNodes(%v) = %v, want %v", tt.b.String(), c, tt.c)
 		}
+	}
+}
+
+func TestCountSharedLeaves(t *testing.T) {
+	tn := &node.Node{Type: node.LeafType, Leaf: node.Leaf{true}}
+	fn := &node.Node{Type: node.LeafType, Leaf: node.Leaf{false}}
+	var tests = []struct {
+		b *ROBDD
+		c int
+	}{
+		{
+			&ROBDD{
+				[]string{"a"},
+				&node.Node{
+					Type:     node.InternalType,
+					Internal: node.Internal{Ply: 0, True: tn, False: tn},
+				},
+			},
+			2,
+		},
+		{
+			&ROBDD{
+				[]string{"a", "b"},
+				&node.Node{
+					Type: node.InternalType,
+					Internal: node.Internal{
+						Ply: 0,
+						True: &node.Node{
+							Type:     node.InternalType,
+							Internal: node.Internal{Ply: 1, True: tn, False: fn},
+						},
+						False: tn,
+					},
+				},
+			},
+			4,
+		},
+	}
+	for _, tt := range tests {
+		c, e := CountNodes(tt.b)
+		if e != nil {
+			t.Errorf("Encountered error in CountNodes(%v): %v", tt.b, e)
+		}
+		if c != tt.c {
+			t.Errorf("CountNodes(%v) = %v, want %v", tt.b.String(), c, tt.c)
+		}
+	}
+}
+
+func TestCountSharedInternal(t *testing.T) {
+	tn := &node.Node{Type: node.LeafType, Leaf: node.Leaf{true}}
+	fn := &node.Node{Type: node.LeafType, Leaf: node.Leaf{false}}
+	in := &node.Node{
+		Type:     node.InternalType,
+		Internal: node.Internal{Ply: 2, True: tn, False: fn},
+	}
+	b := &ROBDD{
+		Vocabulary: []string{"a", "b", "c"},
+		Node: &node.Node{
+			Type: node.InternalType,
+			Internal: node.Internal{
+				Ply:  0,
+				True: tn,
+				False: &node.Node{
+					Type:     node.InternalType,
+					Internal: node.Internal{Ply: 1, True: in, False: in},
+				},
+			},
+		},
+	}
+	ec := 5
+
+	c, e := CountNodes(b)
+	if e != nil {
+		t.Errorf("Encountered error in CountNodes(%v): %v", b, e)
+	}
+	if c != ec {
+		t.Errorf("CountNodes(%v) = %v, want %v", b.String(), c, ec)
 	}
 }
