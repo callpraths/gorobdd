b6e3ac3744f8563d1467f734e665839085629771
callpraths <callpraths@gmail.com>
2017-11-04 18:06:34 -0700

    Reduce test another ply.

--- reduce_test.go
+++ reduce_test.go
@@ -8,15 +8,15 @@ import (
 func ExampleReduceTrivial() {
 	fmt.Println(Reduce(True([]string{})))
 	fmt.Println(Reduce(False([]string{})))
+	fmt.Println(Reduce(True([]string{"a"})))
+	fmt.Println(Reduce(False([]string{"a"})))
+	fmt.Println(Reduce(True([]string{"a", "b"})))
+	fmt.Println(Reduce(False([]string{"a", "b"})))
 	// Output:
 	// T <nil>
 	// F <nil>
-}
-
-func ExampleReduceSkipsLevel() {
-	fmt.Println(Reduce(True([]string{"a"})))
-	fmt.Println(Reduce(False([]string{"a"})))
-	// Output:
+	// T <nil>
+	// F <nil>
 	// T <nil>
 	// F <nil>
 }
